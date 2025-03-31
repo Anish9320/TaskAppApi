23afa8429afbd4c486f4973d68db413724e474e2
Anish <anishdharnidhar07@gmail.com>
2025-03-31 14:45:43 +0530

    evn

--- config/supabaseConfig.js
+++ config/supabaseConfig.js
@@ -1,7 +1,8 @@
 const { createClient } = require("@supabase/supabase-js")
+require("dotenv").config()
 
-const supabaseUrl = 'https://qrbqqcxrmxnxncvfwknr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyYnFxY3hybXhueG5jdmZ3a25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwMjQyMjUsImV4cCI6MjA1NDYwMDIyNX0.KSUl20RhNQkJOCl9ZO1R-FugxEbpOlg8CHjEPqNH9lg'
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseKey = process.env.SUPABASEKEY;
 
 
 const supabase = createClient(supabaseUrl,supabaseKey);
